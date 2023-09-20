5f8307d95facc1e3718aa4ffacecb4ec72a92e0b
Timilehin Adenuga <Timswaggs1@outlook.com>
2023-09-20 15:10:36 +0100

    Feat: Improved logger

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,12 @@
 const errorHandler = require('../middlewares/errorHandler');
-const logger = require('../middlewares/logger');
 
 module.exports = function (app) {
-  app.use(logger)
   app.use('/api/auth', require('./authroute'));
   app.use('/api/pickup-points', require('./pickUpRoutes'));
   app.use('/api/locations', require('./locationRoutes'));
   app.use('/api/routes', require('./routeMgtRoutes'));
   
-  // handle non matching routes
+  // handle non-matching routes
   app.use((req, res, next) => {
     res.status(404).json({
       message: 'Invalid api endpoint',
